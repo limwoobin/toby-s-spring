8b844468bdaa48af91de1e9f86476ca434e4f703
limwoobin <drogba02@naver.com>
2020-12-06 15:45:56 +0900

    git init

---
+++ src/test/java/com/drogba/toby/TobyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.drogba.toby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("toby`s spring");
+	}
+
+}
