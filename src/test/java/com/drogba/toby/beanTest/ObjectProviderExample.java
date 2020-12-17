798a06a3a365c907b4a49004dcf312adc8f7da6c
limwoobin <drogba02@naver.com>
2020-12-17 16:11:51 +0900

    Toby`s Spring
    - ObjectProviderExample

---
+++ src/test/java/com/drogba/toby/beanTest/ObjectProviderExample.java
@@ -0,0 +1,32 @@
+package com.drogba.toby.beanTest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectProviderExample {
+
+    @Bean
+    public BeanClass beanClass() {
+        return new BeanClass("drogba");
+    }
+
+    public static class BeanClass {
+        private final String name;
+
+        public BeanClass(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "BeanClass{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+}
---
+++ src/test/java/com/drogba/toby/beanTest/Provider.java
@@ -0,0 +1,22 @@
+package com.drogba.toby.beanTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Provider {
+
+    public Provider(ObjectFactory<ObjectProviderExample.BeanClass> factory) {
+        ObjectProviderExample.BeanClass object = factory.getObject();
+        System.out.println(object);
+    }
+
+    public Provider(ObjectProvider<ObjectProviderExample.BeanClass> provider) {
+        ObjectProviderExample.BeanClass ifAvailable = provider.getIfAvailable();
+        ObjectProviderExample.BeanClass ifUnique = provider.getIfUnique();
+        System.out.println(ifAvailable);
+        System.out.println(ifUnique);
+    }
+}
