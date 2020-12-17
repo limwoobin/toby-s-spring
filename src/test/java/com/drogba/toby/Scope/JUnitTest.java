d6060613726866d027690c1901076ec37372173e
limwoobin <drogba02@naver.com>
2020-12-17 14:29:27 +0900

    Toby`s Spring
    - Prototype Bean Test

--- src/test/java/com/drogba/toby/Scope/JUnitTest.java
+++ src/test/java/com/drogba/toby/Scope/JUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -34,5 +35,32 @@ public class JUnitTest {
     static class SingletonClientBean {
         @Autowired SingletonBean bean1;
         @Autowired SingletonBean bean2;
+    }
+
+    @Test
+    public void prototypeScope() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(
+            PrototypeBean.class , PrototypeClientBean.class
+        );
+
+        Set<PrototypeBean> bean = new HashSet<>();
+        bean.add(ac.getBean(PrototypeBean.class));
+        assertThat(bean.size() , is(1));
+
+        bean.add(ac.getBean(PrototypeBean.class));
+        assertThat(bean.size() , is(2));
+
+        bean.add(ac.getBean(PrototypeClientBean.class).scopeBean1);
+        assertThat(bean.size() , is(3));
+
+        bean.add(ac.getBean(PrototypeClientBean.class).scopeBean2);
+        assertThat(bean.size() , is(4));
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {}
+    static class PrototypeClientBean {
+        @Autowired PrototypeBean scopeBean1;
+        @Autowired PrototypeBean scopeBean2;
     }
 }
